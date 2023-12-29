302f9f7bea863b0116632d63e6f0eab3a3b1da5b
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2023-12-29 11:26:48 +1100

    chore(docs): got docs site running again after nuxt upgrade

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -38,5 +38,16 @@ export default defineNuxtConfig({
     prerender: {
       ignore: ['/storybook']
     }
+  },
+  // A change in nuxt 3.8.0 means we were getting errors whenever a type was imported without the 'type' keyword
+  // This is a temporary workaround until we can fix all the types
+  // TODO: Add 'type' keyword to all type imports
+  // https://github.com/nuxt/nuxt/releases/tag/v3.8.0
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        verbatimModuleSyntax: false
+      }
+    }
   }
 })
