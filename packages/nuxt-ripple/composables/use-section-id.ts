dpc-sdp/ripple

feat(@dpc-sdp/ripple-tide-api): add section id to logging messages and add debug info

packages/nuxt-ripple/composables/use-section-id.ts
@@ -1,0 +1,9 @@
+import { useRequestHeaders } from '#imports'
+
+export const useSectionId = (): string => {
+  const sectionIdHeaderName = 'section-io-id'
+  const requestHeaders = useRequestHeaders([sectionIdHeaderName])
+  return requestHeaders[sectionIdHeaderName] || 'no section id'
+}
+
+export default useSectionId

dpc-sdp/ripple-framework

packages/nuxt-ripple/composables/use-section-id.ts
@@ -1,0 +1,9 @@
+import { useRequestHeaders } from '#imports'
+
+export const useSectionId = (): string => {
+  const sectionIdHeaderName = 'x-request-id'
+  const requestHeaders = useRequestHeaders([sectionIdHeaderName])
+  return requestHeaders[sectionIdHeaderName] || 'no section id'
+}
+
+export default useSectionId